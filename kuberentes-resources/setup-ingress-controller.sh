fcc2e43bd4696b076915aa090c6e0379b3c17246
Hrvoje <htonkovac@gmail.com>
2019-08-13 15:41:01 +0200

    (k8s) added scripts: setup helm, setup ingress-controller; added ingress resource

--- kuberentes-resources/setup-ingress-controller.sh
+++ kuberentes-resources/setup-ingress-controller.sh
@@ -1,5 +1,5 @@
-helm repo add nginx-stable https://helm.nginx.com/stable
-helm repo update
-helm install nginx-stable/nginx-ingress --name nginx-ingress-controller
+# helm repo add nginx-stable https://helm.nginx.com/stable
+# helm repo update
+helm install stable/nginx-ingress --name nginx-ingress-controller
 
 # kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
